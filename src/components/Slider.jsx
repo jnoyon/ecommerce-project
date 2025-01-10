a3da85424f9fedade22ca21be841d0dab98fa373
jnoyon <jihadur51@gmail.com>
2025-01-10 20:33:56 +0600

    Added Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 export default function Slider() {
   return (
-    <div>Slider</div>
+    <Carousel>
+                <div>
+                    <img src="https://tp.jihadur.com/wp-content/uploads/2025/01/banner.png" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="https://tp.jihadur.com/wp-content/uploads/2025/01/banner-2.png" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="https://tp.jihadur.com/wp-content/uploads/2025/01/banner-2.png" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
   )
 }
